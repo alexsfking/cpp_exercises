1158b6eec581d7a2df74fc75c55e658995f8c1d6
Alex <104585968+alexsfking@users.noreply.github.com>
2023-01-10 23:37:17 +1100

    basic cpp

---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+// hello world
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
---
+++ input_and_output.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// In this challenge, we practice reading input from stdin and printing output to stdout.
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int x,y,z;
+    cin >> x >> y >> z;
+    cout << (x+y+z);
+    return 0;
+}
